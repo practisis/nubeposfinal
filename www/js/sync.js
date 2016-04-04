7c2165b3d10dd91c84d8b418c7ed8aa92440828b
Ruben <rvargas@practisis.com>
2016-04-04 12:16:00 -0500

    Variable de ip para servidor

--- www/js/sync.js
+++ www/js/sync.js
@@ -260,10 +260,11 @@ function ExtraeDatosApi(donde){
 			//localStorage.setItem("con_mesas",ext[0].comanderas);
 			localStorage.setItem("con_mesas",true);
             localStorage.setItem("con_localhost",ext[0].localhost);
+            localStorage.setItem("ip_servidor",ext[0].ipservidor);
 
             var db = window.openDatabase("Database", "1.0", "PractisisMobile", 200000);
 			db.transaction(function(tx){
-              tx.executeSql('UPDATE CONFIG SET pais="'+ext[0].pais+'",id_idioma = "'+ext[0].idioma+'",sin_documento="'+ext[0].documento+'",con_nombre_orden="'+ext[0].orden+'",con_propina="'+ext[0].propina+'",con_tarjeta="'+ext[0].tarjeta+'",con_shop="'+ext[0].shop+'" WHERE id=1',[],function(tx,results){
+              tx.executeSql('UPDATE CONFIG SET pais="'+ext[0].pais+'",id_idioma = "'+ext[0].idioma+'",sin_documento="'+ext[0].documento+'",con_nombre_orden="'+ext[0].orden+'",con_propina="'+ext[0].propina+'",con_tarjeta="'+ext[0].tarjeta+'",con_shop="'+ext[0].shop+'",ip_servidor="'+ext[0].ipservidor+'" WHERE id=1',[],function(tx,results){
   				console.log("actualizada empresa permisos");
   			  });
             },errorCB,successCB);
@@ -890,9 +891,10 @@ function DatosRecurrentes(cual){
 					localStorage.setItem("con_comandas",item.comanderas);
 					localStorage.setItem("con_mesas",true);
                     localStorage.setItem("con_localhost",item.localhost);
+                    localStorage.setItem("ip_servidor",item.ipservidor);
 
 
-					tx.executeSql('UPDATE CONFIG SET nombre="'+item.nombreempresa+'",razon = "'+item.razon+'" , ruc="'+item.ruc+'",telefono ="'+item.telefono+'",direccion="'+item.direccion+'",serie="'+item.serie+'",establecimiento="'+item.establecimiento+'",nombreterminal="'+item.nombreterminal+'",pais="'+item.pais+'",id_idioma = "'+item.idioma+'",sin_documento="'+item.documento+'",con_nombre_orden="'+item.orden+'",con_propina="'+item.propina+'",con_tarjeta="'+item.tarjeta+'",con_shop="'+item.shop+'",con_notasorden="'+item.notas+'",con_comanderas="'+item.comanderas+'",con_localhost="'+item.localhost+'" WHERE id=1',[],function(tx,results){
+					tx.executeSql('UPDATE CONFIG SET nombre="'+item.nombreempresa+'",razon = "'+item.razon+'" , ruc="'+item.ruc+'",telefono ="'+item.telefono+'",direccion="'+item.direccion+'",serie="'+item.serie+'",establecimiento="'+item.establecimiento+'",nombreterminal="'+item.nombreterminal+'",pais="'+item.pais+'",id_idioma = "'+item.idioma+'",sin_documento="'+item.documento+'",con_nombre_orden="'+item.orden+'",con_propina="'+item.propina+'",con_tarjeta="'+item.tarjeta+'",con_shop="'+item.shop+'",con_notasorden="'+item.notas+'",con_comanderas="'+item.comanderas+'",con_localhost="'+item.localhost+'",ip_servidor="'+item.ipservidor+'" WHERE id=1',[],function(tx,results){
 						console.log("actualizada empresa");
 					});
 				}
